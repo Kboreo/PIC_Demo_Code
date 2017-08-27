8fbe79df01bff4ad8534b86632eb297fb4288df2
Kboreo <kboreo@ltu.edu>
2017-08-27 11:42:44 -0400

    Initial Project

---
+++ E16LCDDemo_pic24fj128ga204_pim_1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic24fj128ga204_pim configuration
+CND_ARTIFACT_DIR_pic24fj128ga204_pim=dist/pic24fj128ga204_pim/production
+CND_ARTIFACT_NAME_pic24fj128ga204_pim=E16LCDDemo_pic24fj128ga204_pim_1.X.production.hex
+CND_ARTIFACT_PATH_pic24fj128ga204_pim=dist/pic24fj128ga204_pim/production/E16LCDDemo_pic24fj128ga204_pim_1.X.production.hex
+CND_PACKAGE_DIR_pic24fj128ga204_pim=${CND_DISTDIR}/pic24fj128ga204_pim/package
+CND_PACKAGE_NAME_pic24fj128ga204_pim=e16lcddemopic24fj128ga204pim1.x.tar
+CND_PACKAGE_PATH_pic24fj128ga204_pim=${CND_DISTDIR}/pic24fj128ga204_pim/package/e16lcddemopic24fj128ga204pim1.x.tar
